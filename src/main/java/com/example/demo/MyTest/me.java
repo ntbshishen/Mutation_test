a4bdd9352108b7427db1cd78dcf146ceedd7639d
ntbshishen <1036886215@qq.com>
2023-12-29 12:15:08 +0800

    完成了一些测试

---
+++ src/main/java/com/example/demo/MyTest/me.java
@@ -0,0 +1,10 @@
+package com.example.demo.MyTest;
+
+public class me {
+
+    public void testOne(int b) {
+        if (1 != (-(-Math.abs(b)))) {
+            System.out.println("true");
+        }
+    }
+}
